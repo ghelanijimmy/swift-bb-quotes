59b8e062ed2d93bbf306d21d4aa8df549d48cddb
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-06 09:01:29 -0400

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Jimmy Ghelani on 2023-09-06.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
